d41c4843a293ffde8ce5e9c81dc5a4f257a38a49
Xavier Ho <contact@xavierho.com>
2016-01-30 00:51:12 +1100

    Added more ingredients

--- TheCauldron/Assets/Scripts/IngredientMixer.cs
+++ TheCauldron/Assets/Scripts/IngredientMixer.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class IngredientMixer : MonoBehaviour {
 
+  // Ingredient List
+  public Transform Ham;
+
   // Recipe for testing
   public string PigReceipe = "Ham,Cheese";
   public string RoastRecipe = "Pig,Apple";
@@ -14,6 +17,9 @@ public class IngredientMixer : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
+    if (Input.GetKeyDown(KeyCode.Space)) // FIXME: Just using spacebar to trigger
+     {
+         var go = Instantiate(Ham, transform.position + new Vector3(0, 3, 0), transform.rotation);
+     }
 	}
 }
