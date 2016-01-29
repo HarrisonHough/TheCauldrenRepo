2266a9dc726b86aa50f89f88e3e3124883efeaf0
Xavier Ho <contact@xavierho.com>
2016-01-30 10:46:30 +1100

    Added dummy logic to mix ingredients

--- TheCauldron/Assets/Scripts/IngredientMixer.cs
+++ TheCauldron/Assets/Scripts/IngredientMixer.cs
@@ -15,26 +15,37 @@ public class IngredientMixer : MonoBehaviour {
   // Particle Emitter
   public Transform FlashySmoke;
 
+  // FIXME: Ingredient tracker
+  private ArrayList ingredients;
+
 	// Use this for initialization
 	void Start () {
-
+    ingredients = new ArrayList();
 	}
 
   Object MixIngredient () {
-    if (true) {// Array Contains required ingredient
-      return Instantiate(Pig, transform.position + new Vector3(0, 3, 0), transform.rotation);
+    ingredients.Add("Ham");
+    ingredients.Add("Cheese");
+    Debug.Log(ingredients);
+    if (ingredients.Contains("Ham") && ingredients.Contains("Cheese")) {// Array Contains required ingredient
+
+      // Play cauldron flashy smoke effect
+      var particleSystems = GetComponentsInChildren<ParticleSystem>();
+      foreach (ParticleSystem particles in particleSystems) {
+        particles.Play();
+      }
+      ingredients.Clear();
+      return Instantiate(Pig, transform.position + new Vector3(0, 1, 0), transform.rotation);
+
     }
+    return null;
   }
 
 	// Update is called once per frame
 	void Update () {
     if (Input.GetKeyDown(KeyCode.Space)) // FIXME: Just using spacebar to trigger for testing
     {
-      // var newIngredient = MixIngredient();
-      var particleSystems = GetComponentsInChildren<ParticleSystem>();
-      foreach (ParticleSystem particles in particleSystems) {
-        particles.Play();
-      }
+      var newIngredient = MixIngredient();
     }
 	}
 }
