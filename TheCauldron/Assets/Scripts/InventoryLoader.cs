2e199dc62532670ab267bbff618649332bb83619
Xavier Ho <contact@xavierho.com>
2016-01-31 10:25:36 +1100

    Started working on inventory loading

---
+++ TheCauldron/Assets/Scripts/InventoryLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+
+  public Transform SpawnLocation;
+
+	// Use this for initialization
+	void Start () {
+    Spawn();
+	}
+
+  public void Spawn () {
+    Instantiate(gameObject, SpawnLocation.position, SpawnLocation.rotation);
+  }
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
