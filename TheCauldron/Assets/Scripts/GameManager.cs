f59f5d5daa0f8bd69c9e77972daa9af8a03a2f9a
Jacs <developer.jacs@gmail.com>
2016-01-30 17:24:31 +1100

    Actually added the script this time

---
+++ TheCauldron/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static bool gameOver;
+	public static int level;
+	public static bool wonLevel;
+
+	void Start() {
+		gameOver = false;
+		level = 1;
+		wonLevel = false;
+	}
+
+	public static void SetGameOver(bool won) {
+		gameOver = true;
+		wonLevel = won;
+	}
+
+}
