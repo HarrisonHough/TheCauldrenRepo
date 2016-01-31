9b68e1fb8c708074d77fa171b1dbd2ff02a35c2a
Xavier Ho <contact@xavierho.com>
2016-01-31 12:31:12 +1100

    MultiprojectilesPizza

--- TheCauldron/Assets/Scripts/IngredientMixer.cs
+++ TheCauldron/Assets/Scripts/IngredientMixer.cs
@@ -11,6 +11,7 @@ public class IngredientMixer : MonoBehaviour {
   public GameObject Pig;
   public GameObject DragonClaw;
   public GameObject Pizza;
+  public GameObject Potion;
 
   // Particle Emitter
   public ParticleSystem FlashySmoke;
@@ -19,49 +20,54 @@ public class IngredientMixer : MonoBehaviour {
   // DUMMY: Ingredient tracker
   private ArrayList ingredients;
 
-  private GameObject Projectile;
+  private ArrayList Projectiles;
   private GameObject Target;
   private float waitProjectile;
 
 	// Use this for initialization
 	void Start () {
     ingredients = new ArrayList();
-    Projectile = null;
+    Projectiles = new ArrayList();
 	}
 
   public void AddIngredient (string ingredient) {
     ingredients.Add(ingredient);
-    Projectile = MixIngredient();
+    MixIngredient();
     MagicalSmoke.Play();
-    if (Projectile) {
-      Rigidbody rb = Projectile.GetComponent<Rigidbody>();
-      rb.isKinematic = false;
-    }
   }
 
-  GameObject MixIngredient () {
-    if (ingredients.Contains("Ham") && ingredients.Contains("Cheese")) {
+  void MixIngredient () {
+    if (ingredients.Contains("Potion")) {
+      ingredients.Remove("Potion");
+      SpawnIngredient(Potion);
+      FlashySmoke.Play();
+    }
+    else if (ingredients.Contains("Ham") && ingredients.Contains("Cheese")) {
       ingredients.Remove("Ham");
       ingredients.Remove("Cheese");
-      return SpawnIngredient(Pig);
+      SpawnIngredient(Pig);
+      FlashySmoke.Play();
     }
-    if (ingredients.Contains("Eye") && ingredients.Contains("Potion")) {
+    else if (ingredients.Contains("Eye") && ingredients.Contains("Potion")) {
       ingredients.Remove("Eye");
       ingredients.Remove("Potion");
-      return SpawnIngredient(DragonClaw);
+      SpawnIngredient(DragonClaw);
+      FlashySmoke.Play();
     }
-    if (ingredients.Contains("Potion") && ingredients.Contains("Pizza")) {
+    else if (ingredients.Contains("Pizza")) {
       ingredients.Remove("Pizza");
-      ingredients.Remove("Potion");
-      return SpawnIngredient(Pizza);
+      SpawnIngredient(Pizza);
+      SpawnIngredient(Pizza);
+      SpawnIngredient(Pizza);
+      FlashySmoke.Play();
     }
-    return null;
   }
 
-  GameObject SpawnIngredient (GameObject ingredient) {
-    FlashySmoke.Play();
-    ingredients.Clear();
-    return (GameObject) Instantiate(ingredient, transform.position + new Vector3(0, 1, 0), transform.rotation * Quaternion.Euler(Random.Range(-10,10),0,0));
+  void SpawnIngredient (GameObject ingredient) {
+    GameObject Projectile = (GameObject) Instantiate(ingredient, transform.position + new Vector3(0, 1, 0), transform.rotation * Quaternion.Euler(Random.Range(-10,10),0,0));
+    Rigidbody rb = Projectile.GetComponent<Rigidbody>();
+    rb.isKinematic = false;
+    Projectiles.Add(Projectile);
   }
 
   void GetClosestEnemy () {
@@ -83,14 +89,11 @@ public class IngredientMixer : MonoBehaviour {
       targetPos = Target.transform.position;
     }
 
-    if (Projectile && Target) {
-      Projectile.GetComponent<Ingredient>().FlyTo(targetPos + new Vector3(0, 3, 0));
-      Projectile = null;
-    }
-    if (Input.GetKeyDown(KeyCode.Space)) // DUMMY: Just using spacebar to trigger for testing
-    {
-      // if (Projectile)
-      // Projectile.GetComponent<Ingredient>().FlyTo(Target.transform.position);
+    foreach(GameObject Projectile in Projectiles) {
+      if (Target) {
+        Projectile.GetComponent<Ingredient>().FlyTo(targetPos + new Vector3(0, 3, 0));
+      }
+      Projectiles.Remove(Projectile);
     }
 	}
 }
